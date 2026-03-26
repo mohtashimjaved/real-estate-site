418f67359a7ec5a46eb14e88973073e3f1ad7bb1
Mohtashim Javed <hafizmohtashim3157@gmail.com>
2026-03-26 20:30:17 +0500

    updated

---
+++ db/index.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.dnb8z7m.mongodb.net/?appName=${process.env.DB_NAME}`
+
+mongoose.connect(url)
+
+export default mongoose;
--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 import express from "express";
+import mongoose from "./db/index.js";
 
 const app = express()
-
 app.use(express.json())
-let data = []
 
-app.get("/", (req, res) => {
-    res.send(data)
-    res.status(200)
+mongoose.connection.on("open", () => {
+    console.log("DB connected");
 })
 
-app.post("/data", (req, res) => {
-    console.log(req.body[0]);
-    data.push(req.body[0])
-    res.send({user: req.body[0]  , message: "user succesfully added"})
+mongoose.connection.on("error", (err) => {
+    console.log("DB error occured" , err);
 })
+
+
+// let data = []
+
+// app.get("/", (req, res) => {
+//     res.send(data)
+//     res.status(200)
+// })
+
+// app.post("/data", (req, res) => {
+//     console.log(req.body[0]);
+//     data.push(req.body[0])
+//     res.send({ user: req.body[0], message: "user successfully added" })
+// })
+
+// app.delete("/data-delete", (req, res) => {
+//     console.log("deleted successfully")
+// })
+
+// app.put("/edit-data", (req, res) => {
+
+// })
 
 app.listen(3000, () => {
     console.log("testing");
