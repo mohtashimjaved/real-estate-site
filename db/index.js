be04b42b72fc602dcf16fc5224266976ec4b13d8
Mohtashim Javed <hafizmohtashim3157@gmail.com>
2026-03-31 00:09:25 +0500

    updated

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.dnb8z7m.mongodb.net/?appName=${process.env.DB_NAME}`
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.dnb8z7m.mongodb.net/real-estate-project?appName=${process.env.DB_NAME}`
 
 mongoose.connect(url)
 
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-import express from "express";
+import express, { Router } from "express";
 import mongoose from "./db/index.js";
+import User from "./model/index.js";
+import bcrypt from "bcrypt"
+import helmet from "helmet";
+import cors from "cors"
+import router from "./routes/index.js";
 
 const app = express()
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use("api", router)
 
 mongoose.connection.on("open", () => {
     console.log("DB connected");
@@ -11,6 +19,61 @@ mongoose.connection.on("open", () => {
 mongoose.connection.on("error", (err) => {
     console.log("DB error occured" , err);
 })
+
+
+const getUser = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.send({ status: 200, message: "User data received successfully", user: users })
+
+    } catch (err) {
+        res.status(500).send({ error: err })
+    }
+}
+app.get("/" , getUser)
+
+const createUser = async (req, res) => {
+    try {
+        console.log(req.body.password)
+        const password = bcrypt.hashSync(req.body.password, 10);
+        console.log(password, "password")
+        const user = await User.create({
+            ...req.body,
+            password
+        })
+        const data = user.toObject();
+        console.log(data.password, "data.password" )
+        delete data.password
+        res.send({ status: 200, message: "User created successfully", user: data})
+
+    } catch (err) {
+        res.status(500).send({ error: err })
+    }
+}
+
+app.post("/post", createUser)
+
+const loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body
+        const user = await User.findOne({email})
+        if(user) {
+            const checkPassword = bcrypt.compareSync(password, user.password);
+            if(checkPassword) {
+                res.send({ status: 200, message: "User logged in succesfully", id: user})
+            } else {
+                res.send({ status: 401, message: "Incorrect password" })
+            }
+        } else {
+            res.status(404).send({ error: "User not found" })
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ error: err })
+    }
+}
+
+app.post("/login", loginUser)
 
 
 // let data = []
